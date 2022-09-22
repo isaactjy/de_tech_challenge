14e74a7b33a42d5b451b11532ffd7f69f28fcc13
Isaac Teo <teojyiz@gmail.com>
2022-09-22 16:30:40 +0800

    Add new cases (derived) and change datasource

--- section4/dashboard.py
+++ section4/dashboard.py
@@ -9,21 +9,94 @@ import pandas as pd
 start_date = '2020-01-01T00:00:00Z'
 end_date = datetime.now().isoformat()
 
-response = requests.get(f'https://api.covid19api.com/country/singapore?from={start_date}&to={end_date}')
+response = requests.get(f'https://api.covid19api.com/country/singapore/status/confirmed?from={start_date}&to={end_date}')
 df = pd.DataFrame.from_dict(response.json(), orient = 'columns')
+df['NewCases'] = df['Cases'].shift(1)
+df['NewCases'] = df['NewCases'].fillna(0)
+df['NewCases'] = df['Cases'] - df['NewCases']
 df.to_csv('covid19_sg.csv')
 
 app = Dash(__name__)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-fig = px.scatter(df, x="Date", y="Confirmed")
+total_cases_fig = px.bar(df, x="Date", y="Cases").update_layout(
+    xaxis = {
+        'rangeselector': {
+            'buttons': [
+                {
+                    'count': 1,
+                    'label': '1m',
+                    'step': 'month',
+                    'stepmode': 'backward'
+                },
+                {
+                    'count': 3,
+                    'label': '3m',
+                    'step': 'month',
+                    'stepmode': 'backward'
+                },
+                {
+                    'count': 6,
+                    'label': '6m',
+                    'step': 'month',
+                    'stepmode': 'backward'
+                },
+                {
+                    'count': 1,
+                    'label': 'YTD',
+                    'step': 'year',
+                    'stepmode': 'backward'
+                },
+            ]
+        },
+        'rangeslider': {'visible': True}
+    }
+)
+
+new_cases_fig = px.bar(df, x="Date", y="NewCases").update_layout(
+    xaxis = {
+        'rangeselector': {
+            'buttons': [
+                {
+                    'count': 1,
+                    'label': '1m',
+                    'step': 'month',
+                    'stepmode': 'backward'
+                },
+                {
+                    'count': 3,
+                    'label': '3m',
+                    'step': 'month',
+                    'stepmode': 'backward'
+                },
+                {
+                    'count': 6,
+                    'label': '6m',
+                    'step': 'month',
+                    'stepmode': 'backward'
+                },
+                {
+                    'count': 5,
+                    'label': 'YTD',
+                    'step': 'year',
+                    'stepmode': 'backward'
+                },
+            ]
+        },
+        'rangeslider': {'visible': True}
+    }
+)
 
 app.layout = html.Div(children=[
     html.H2(children='Confirmed Covid Cases in Singapore Over Time'),
     dcc.Graph(
-        id = 'cases-graph',
-        figure = fig
+        id = 'total-cases-graph',
+        figure = total_cases_fig
+    ),
+    dcc.Graph(
+        id = 'new-cases-graph',
+        figure = new_cases_fig
     )
 ])
 
